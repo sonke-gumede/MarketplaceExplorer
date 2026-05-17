a4f74bbc92b6ef1f46699dbcd5c700aa1a453179
Sonke Gumede <ns.gumede2@gmail.com>
2026-05-18 01:52:37 +0200

    feat: configure product store

---
+++ store/useFilterStore.ts
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+
+export type SortOption = "price_asc" | "price_desc" | "rating_desc" | "";
+
+interface FilterState {
+  search: string;
+  debouncedSearch: string;
+  category: string;
+  sort: SortOption;
+  setSearch: (search: string) => void;
+  setDebouncedSearch: (search: string) => void;
+  setCategory: (category: string) => void;
+  setSort: (sort: SortOption) => void;
+  reset: () => void;
+}
+
+export const useFilterStore = create<FilterState>((set) => ({
+  search: "",
+  debouncedSearch: "",
+  category: "",
+  sort: "",
+  setSearch: (search) => set({ search }),
+  setDebouncedSearch: (debouncedSearch) => set({ debouncedSearch }),
+  setCategory: (category) => set({ category }),
+  setSort: (sort) => set({ sort }),
+  reset: () => set({ search: "", debouncedSearch: "", category: "", sort: "" }),
+}));
