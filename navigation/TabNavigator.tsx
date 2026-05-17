0cfd415a4d67c85a6bc067623fe9aec94c51d89c
Sonke Gumede <ns.gumede2@gmail.com>
2026-05-18 01:02:48 +0200

    fix: hide tabs header

--- navigation/TabNavigator.tsx
+++ navigation/TabNavigator.tsx
@@ -21,7 +21,7 @@ const Tab = createBottomTabNavigator<TabParamList>();
 
 export default function TabNavigator() {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator screenOptions={{ headerShown: false }}>
       <Tab.Screen
         name="Home"
         component={HomeScreen}
