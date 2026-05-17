16df4983cc5b3c52308c5affb5e6fa9863d73b65
Sonke Gumede <ns.gumede2@gmail.com>
2026-05-18 01:53:43 +0200

    feat: add QueryClient Provider

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { ThemeProvider } from 'styled-components/native';
@@ -5,20 +6,31 @@ import { StatusBar } from 'expo-status-bar';
 import AppTheme from './theme';
 import RootNavigator from './navigation/RootNavigator';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 2,
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+});
+
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <ThemeProvider theme={AppTheme}>
-        <SafeAreaView
-          style={{ flex: 1, backgroundColor: AppTheme.colors.default }}
-          edges={['top', 'left', 'right']}
-        >
-          <NavigationContainer>
-            <RootNavigator />
-          </NavigationContainer>
-        </SafeAreaView>
-        <StatusBar style="auto" />
-      </ThemeProvider>
-    </SafeAreaProvider>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <ThemeProvider theme={AppTheme}>
+          <SafeAreaView
+            style={{ flex: 1, backgroundColor: AppTheme.colors.default }}
+            edges={['top', 'left', 'right']}
+          >
+            <NavigationContainer>
+              <RootNavigator />
+            </NavigationContainer>
+          </SafeAreaView>
+          <StatusBar style="auto" />
+        </ThemeProvider>
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
