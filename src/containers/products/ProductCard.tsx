3be20c4c6346d6efad9826bd5ece78f9cc59c78b
Sonke Gumede <ns.gumede2@gmail.com>
2026-05-18 12:04:59 +0200

    chore: add onPress event

--- src/containers/products/ProductCard.tsx
+++ src/containers/products/ProductCard.tsx
@@ -1,10 +1,16 @@
 import { memo } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import ProductCardUI from "../../components/cards/ProductCard";
 import { Product } from "../../api/products";
 import { useCartStore } from "../../store/useCartStore";
 import { isPremium, isLowStock, canAddToCart } from "../../utils/productRules";
+import { DashboardStackParamList } from "../../navigation/DashboardNavigator";
+
+type Nav = NativeStackNavigationProp<DashboardStackParamList>;
 
 const ProductCard = memo(({ product }: { product: Product }) => {
+  const navigation = useNavigation<Nav>();
   const addItem = useCartStore((s) => s.addItem);
   const cartQuantity = useCartStore(
     (s) => s.items.find((i) => i.product.id === product.id)?.quantity ?? 0
@@ -18,6 +24,7 @@ const ProductCard = memo(({ product }: { product: Product }) => {
       lowStock={isLowStock(product)}
       eligible={canAddToCart(product)}
       onAddToCart={() => addItem(product)}
+      onPress={() => navigation.navigate("ProductDetail", { productId: product.id })}
     />
   );
 });
