e12827650e633fa5ba1fe48abb2083528abb4129
Sonke Gumede <ns.gumede2@gmail.com>
2026-05-18 17:34:41 +0200

    fix: upadte splash

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
+import { useCallback, useEffect, useState } from 'react';
+import { View } from 'react-native';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { ThemeProvider } from 'styled-components/native';
 import { StatusBar } from 'expo-status-bar';
+import * as SplashScreen from 'expo-splash-screen';
 import AppTheme from './src/theme';
 import RootNavigator from './src/navigation/RootNavigator';
+
+SplashScreen.preventAutoHideAsync();
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,21 +21,35 @@ const queryClient = new QueryClient({
 });
 
 export default function App() {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    setReady(true);
+  }, []);
+
+  const onLayout = useCallback(async () => {
+    if (ready) await SplashScreen.hideAsync();
+  }, [ready]);
+
+  if (!ready) return null;
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <SafeAreaProvider>
-        <ThemeProvider theme={AppTheme}>
-          <SafeAreaView
-            style={{ flex: 1, backgroundColor: AppTheme.colors.default }}
-            edges={['top', 'left', 'right']}
-          >
-            <NavigationContainer>
-              <RootNavigator />
-            </NavigationContainer>
-          </SafeAreaView>
-          <StatusBar style="auto" />
-        </ThemeProvider>
-      </SafeAreaProvider>
-    </QueryClientProvider>
+    <View style={{ flex: 1 }} onLayout={onLayout}>
+      <QueryClientProvider client={queryClient}>
+        <SafeAreaProvider>
+          <ThemeProvider theme={AppTheme}>
+            <SafeAreaView
+              style={{ flex: 1, backgroundColor: AppTheme.colors.default }}
+              edges={['top', 'left', 'right']}
+            >
+              <NavigationContainer>
+                <RootNavigator />
+              </NavigationContainer>
+            </SafeAreaView>
+            <StatusBar style="auto" />
+          </ThemeProvider>
+        </SafeAreaProvider>
+      </QueryClientProvider>
+    </View>
   );
 }
